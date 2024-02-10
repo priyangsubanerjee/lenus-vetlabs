fa1b7f54706d7c99d39c615c98ce81f4218c9704
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-02-10 19:12:22 +0530

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full h-full">
+      <h2>This site is under development</h2>
+    </main>
+  );
+}
